cf1083cfa4e8429cdabfdd036100b3236c4a0ca8
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-06-10 13:33:12 +0200

    toolbox_run: be more careful with stdin
    Make sure the initial `toolbox` run doesn't get stdin, and that the
    primary `podman` run does.

--- src/boxi/toolbox_run.py
+++ src/boxi/toolbox_run.py
@@ -11,12 +11,14 @@ def main():
     # We do this first, for two reasons:
     #   - give toolbox a chance to start the container if it's not running
     #   - get the exact environment that toolbox would have sent in
-    env = subprocess.check_output(['toolbox', 'run', '--container', args.container, 'env', '-0'])
+    env = subprocess.check_output(['toolbox', 'run', '--container', args.container, 'env', '-0'],
+            stdin=subprocess.DEVNULL)
     env_args = [b'--env=' + key_val for key_val in env.split(b'\0') if key_val]
 
     os.execvp('podman', [
         'podman', 'exec',
 
+        '--interactive',
         '--preserve-fds=1',
 
         '--user', os.getlogin(),
